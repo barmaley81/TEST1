086a87aebce054a7b510ae3d2b0890e76f7b6d9f
Leha <erofeev.alleksey@list.ru>
2016-11-22 23:41:36 +0400

    commit3

--- src/t1/t1_1.java
+++ src/t1/t1_1.java
@@ -3,8 +3,8 @@ package t1;
 public class t1_1 {
 
 	public static void main(String[] args) {
-		System.out.println("t1   t1");
-		// первое изменение
+		System.out.println("t1   t1    t1");
+		// второе изменение
 
 	}
 
